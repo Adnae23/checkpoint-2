6a44d4cce410f66c916fba2cd43cb4506ea17632
Adnae23 <anais23ard@hotmail.fr>
2022-03-18 10:18:06 +0100

    creation app et ajout component header.js

---
+++ react-game/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.name}</h1>
+            <h4>Bienvenue dans la Team !!!! a revoir pour etre drole. Je sais, je sais... encore un message de bienvenue,pff</h4>
+        </div>
+    );
+};
+
+export default Header;
